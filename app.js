37d35935d6c2390361b713b02b655fad5bbf9afc
RachitaPant <rachitap89@gmail.com>
2024-02-13 00:08:27 +0530

    testing different http methods

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+//app.use(express.json());
 
-app.get('/', (req, res) => {
+/*app.get('/', (req, res) => {
   res
     .status(200)
     .json({ message: 'hello from the server side!', app: 'Natours' });
 });
 app.post('/', (req, res) => {
   res.send('You can post to this endpoint...');
+});*/
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId });
+  res.send('Done..');
 });
 const port = 3000;
 
